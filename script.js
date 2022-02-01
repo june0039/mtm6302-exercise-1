d2c081e4ef4d2f285d758c55979810e668e95873
june0039 <74568248+june0039@users.noreply.github.com>
2022-02-01 14:28:07 -0500

    java

---
+++ script.js
@@ -0,0 +1,9 @@
+/* This is the Java Script page For the 100 Emoji's code Program */
+ /*Emoji Div Boxes and Emojis on display*/  
+ 
+<p style="text-align: center;">
+<span style="font-size: 5rem;">&#129409;</span><br>
+<code>129409</code>
+</p>
+
+    /* This is the For loop to add emoji's to the Page */
