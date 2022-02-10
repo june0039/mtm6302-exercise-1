750331fbbf7882c3210cc84cbfefeab15f62a925
june0039 <74568248+june0039@users.noreply.github.com>
2022-02-10 13:14:35 -0500

    Completes the assignment

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # mtm6302-exercise-1
-In this project for Web Design Class #3 we are to use HTML code to display 100 different emoji's on the screen.
+
+In this project for Web Design Class #3 we are to use HTML code to display 100 different emoji's on the screen. We put the code into Javascript by getting the inner html and using a for loop to display 100 emojis dynamically on the page! We also use the css file and code to make the emojis aligned, colorful and more appealling.
 
 By: Julie Juneau
 Student Id: 040930994
--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-/* This is the Java Script page For the 100 Emoji's code Program */
- /*Emoji Div Boxes and Emojis on display*/  
+/* This is the Java Script page For the 100 Emoji's code Program to display 
+100 Emoji Div Boxes and Emojis on display*/
  
-<p style="text-align: center;">
-<span style="font-size: 5rem;">&#129409;</span><br>
-<code>129409</code>
-</p>
+/*Setting the variable emojis by getting the id*/
+const $emojis = document.getElementById('emojis');
 
-    /* This is the For loop to add emoji's to the Page */
+
+/* This is the For loop to add emoji's to the Page up to 100 emojis */
+
+for (let i = 129409; i < 129509; i++) {
+   
+/* This is the code to inject the inner HTML into the javascript page */
+    
+    $emojis.innerHTML += 
+ `<div class= "emoji" style="text-align: center;">
+<span style = "font-size: 4rem;">&#${i}</span>
+<code>${i}</code>
+</div>`
+
+    
+}
